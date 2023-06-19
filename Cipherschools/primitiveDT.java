37b46c04178ed1fb4a0f02ada670ca16572ec523
Arge GangaPrasad <argegangaprasad@gmail.com>
2023-06-19 12:05:47 +0530

    version 2,make some changes

--- Cipherschools/primitiveDT.java
+++ Cipherschools/primitiveDT.java
@@ -2,12 +2,16 @@ import java.lang.*;
 class primitiveDT {
  
  public static void main(String args[]){
-   int X,Y;
    int x=5;
    int y=7;
-   System.out.println("X is: " + X);
-   System.out.println("Y is: " + Y);
+   System.out.println("X is: " + x);
+   System.out.println("Y is: " + y);
+    
+   byte b=10;
+   System.out.println("B is " + b);
 
+   int i=b;
+   System.out.println("I is: "+ i);
      }
 
 }
