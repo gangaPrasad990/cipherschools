0b59d46515b89884a98ed16d2c57ddb6ed666775
Arge GangaPrasad <argegangaprasad@gmail.com>
2023-06-20 00:27:06 +0530

    version 4,make some changes

---
+++ Cipherschools/Lecture7- Counter flow/controlflow.java
@@ -0,0 +1,60 @@
+import java.lang.*;
+class student{
+    int marks;
+    String name,section;
+    boolean checkpass()
+    {
+        if(marks>=40){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+public class controlflow {
+
+    int findMax(int a,int b, int c){
+           if(a>b){
+            if(a>c){
+                System.out.println("a");
+            }
+            else
+            {
+                System.out.println("c");
+            }
+        }
+        else{
+            if(b>c)
+            {
+                System.out.println("b");
+            }
+            else
+            {
+                System.out.println("c");
+            }
+        }
+       return 0; 
+    }
+
+
+    public static void main(String args[]){
+        student s1=new student();
+        s1.name="prasad";
+        s1.marks=95;
+        s1.section="BG";
+
+        student s2=new student();
+        s2.name="ram";
+        s2.marks=3;
+        s2.section="BG";
+
+        System.out.println(s1.checkpass());
+        System.out.println(s2.checkpass());
+
+
+        controlflow cf=new controlflow();
+        System.out.println(cf.findMax(10,5,15));
+
+    }
+}
---
+++ Cipherschools/Lecture7- Counter flow/switch.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+class controlflow1{
+    public static void main(String args[]){
+    char a='c';
+    switch(a){
+        case'a':
+        System.out.println("I am A");
+        break;
+        case'b':
+        System.out.println("I am B");
+        break;
+        case'c':
+        System.out.println("I am C");
+        break;
+        default:
+        System.out.println("I am not A,b,c");
+    }
+
+    }
+}
