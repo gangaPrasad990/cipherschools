8d0303746cee5c508e55f9bc7dcb13791d0309d5
Arge GangaPrasad <argegangaprasad@gmail.com>
2023-06-19 10:59:11 +0530

    version 2,make some changes

---
+++ Cipherschools/primitiveDT.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+class primitiveDT {
+ 
+ public static void main(String args[]){
+   int X,Y;
+   int x=5;
+   int y=7;
+   System.out.println("X is: " + X);
+   System.out.println("Y is: " + Y);
+
+     }
+
+}
+
+
